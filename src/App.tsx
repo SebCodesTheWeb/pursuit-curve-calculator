5a5f72ae74c010802cb2691babd3c92af812ebca
Sebastian Delgado <sebastian.delgado@mediatool.com>
2022-07-14 19:01:41 +0200

    added basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
 import Coordinates from './Coordinates'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
-
-
-
-
-
-
-
-
-
-
   return (
+  <ChakraProvider>
     <div className="App">
       < Coordinates />
     </div>
+  </ChakraProvider>
   );
 }
 
